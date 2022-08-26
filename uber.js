8e9285114a6e3df9d7f1bbbf37afa5e0281b597d
Murgesh Ekunde <mekunde@gmail.com>
2022-08-26 20:09:24 +0530

    Update uber.js

--- uber.js
+++ uber.js
@@ -1,18 +1,18 @@
 //write a class to calculate the uber price.
 
-class uberFarePrice {
-    constructor(base_fare, minutes, distance, fees) {
-        this.base_fare = base_fare;
+class uberFarePrice {                                       //created class name as uberFarePrice
+    constructor(base_fare, minutes, distance, fees) {       //created constructor with properties as base_fare, minutes, distance, fees
+        this.base_fare = base_fare;                         //setting values to properties
         this.minutes = minutes;
         this.distance =distance;
         this.fees= fees;
     }
     getFare(cpm=0.2, cpd=20){        //cpm= cost per minute, cpd = cost per distance
-        var fare = this.base_fare + (cpm * this.minutes) + (this.distance*cpd) + this.fees
+        var fare = this.base_fare + (cpm * this.minutes) + (this.distance * cpd) + this.fees      //formula to calculate fare
         console.log (`Fare for ride is: ${fare}`)
     }  
 }
 
 let ride1 = new uberFarePrice(40, 25, 10, 15)
 
-ride1.getFare();
+ride1.getFare();        //calling method to get fare
